b25072ae7c4060c17c0efaf220828fa49c18e931
Jason Wang <jinqu7777@163.com>
2019-04-23 22:48:02 +0800

    global error handler

--- src/main/java/com/jw/exceptionHandler/GlobalErrorInfoHandler.java
+++ src/main/java/com/jw/exceptionHandler/GlobalErrorInfoHandler.java
@@ -8,22 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class GlobalErrorInfoHandler  extends ResponseEntityExceptionHandler {
-
-//    @ExceptionHandler(value = GlobalErrorInfoException.class)
-//    public ResultBody errorHandlerOverJson(HttpServletRequest request,
-//                                           GlobalErrorInfoException exception) {
-//        ErrorInfoInterface errorInfo = exception.getErrorInfo();
-//        ResultBody result = new ResultBody(errorInfo);
-//        return result;
-//    }
+public class GlobalErrorInfoHandler extends ResponseEntityExceptionHandler {
 
     /**
      * Handles EntityNotFoundException. Created to encapsulate errors with more detail than javax.persistence.EntityNotFoundException.

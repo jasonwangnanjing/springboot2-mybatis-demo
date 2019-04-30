02d059db691850db8bf73232d90c73cf03f181b1
Jason Wang <jinqu7777@163.com>
2019-04-30 18:06:15 +0800

    spring security with JWT

--- src/main/java/com/jw/config/SecurityConfig.java
+++ src/main/java/com/jw/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.jw.config;
 
 
+import com.jw.security.CustomAuthenticationProvider;
+import com.jw.security.JWTAuthenticationFilter;
+import com.jw.security.JWTLoginFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,17 +24,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected  void configure(HttpSecurity http) throws Exception{
 
 //  允许所有用户访问"/"和"/index.html"
-        http.authorizeRequests()
-                .antMatchers("/", "/index.html").permitAll()
-                .anyRequest().authenticated()   // 其他地址的访问均需验证权限
+        // 关闭csrf验证
+        http.csrf().disable()
+                // 对请求进行认证
+                .authorizeRequests()
+                // 所有 / 的所有请求 都放行
+                .antMatchers("/").permitAll()
+                // 所有 /login 的POST请求 都放行
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                // 添加权限检测
+                .antMatchers("/order/*").hasAuthority("AUTH_WRITE")
+                // 角色检测
+                .antMatchers("/user").hasRole("ADMIN")
+                // 所有请求需要身份认证
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login.html")   //  登录页
-                .failureUrl("/login-error.html").permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/index.html");
+                // 添加一个过滤器 所有访问 /login 的请求交给 JWTLoginFilter 来处理 这个类处理所有的JWT相关内容
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // 添加一个过滤器验证其他请求的Token是否合法
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
 
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 使用自定义身份验证组件
+        auth.authenticationProvider(new CustomAuthenticationProvider());
 
     }
 

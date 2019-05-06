09241c64f141025adca97e986dcdb1bc693559a8
Jason Wang <jinqu7777@163.com>
2019-05-06 16:31:22 +0800

    security config

--- src/main/java/com/jw/config/SecurityConfig.java
+++ src/main/java/com/jw/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.jw.security.JWTAuthenticationFilter;
 import com.jw.security.JWTLoginFilter;
 import com.jw.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -33,26 +32,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        // 关闭csrf验证
+        // close csrf
         http.csrf().disable()
-                // 对请求进行认证
+                // authorize request
                 .authorizeRequests()
-                // 所有 /login 的POST请求 都放行
+                // allow post method to get token
                 .antMatchers(HttpMethod.POST, "/token").permitAll()
-                // 添加权限检测
+                // add security check for certain url
                 .antMatchers("/order/*").hasAuthority("AUTH_WRITE")
-                // 角色检测
+                // check role for certain url
                 .antMatchers("/user").hasRole("ADMIN")
-                // 所有请求需要身份认证
+                // any other request need authentication
                 .anyRequest().authenticated()
                 .and()
-                // 添加一个过滤器 所有访问 /login 的请求交给 JWTLoginFilter 来处理 这个类处理所有的JWT相关内容
+                // to get token, it need use special filter which validate user name password
                 .addFilterBefore(new JWTLoginFilter("/token", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
-                // 添加一个过滤器验证其他请求的Token是否合法
+                // other request need validate token
                 .addFilterBefore(new JWTAuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
-
     }
 
     @Override
@@ -60,8 +58,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // 使用自定义身份验证组件
         auth.authenticationProvider(new CustomAuthenticationProvider(sysUserService, bCryptPasswordEncoder));
     }
-
-
-
 
 }
